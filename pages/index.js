3282f367b816f0bc44e9041149006c7c27caf3b7
GabeZake <48073539+GabeZake@users.noreply.github.com>
2022-04-27 13:10:34 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hello, World. My Name is Gabriel Simao." />
+        <img src="pic_trulli.jpg">
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
